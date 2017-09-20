0c0cfa3d2dd2d466a3adc7bc32eedd7912445e90
dariusPan <pjm.darius@gmail.com>
2017-09-20 23:35:39 +0800

    FC

---
+++ lab1.ip_user_files/sim_scripts/zynq_design_1/ies/run.f
@@ -0,0 +1,85 @@
+-makelib ies/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2016.3/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2016.3/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies/xpm \
+  "C:/Xilinx/Vivado/2016.3/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies/processing_system7_bfm_v2_0_5 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/2527/hdl/processing_system7_bfm_v2_0_vl_rfs.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_processing_system7_0_0/sim/zynq_design_1_processing_system7_0_0.v" \
+-endlib
+-makelib ies/blk_mem_gen_v8_3_4 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/59b0/simulation/blk_mem_gen_v8_3.v" \
+-endlib
+-makelib ies/axi_bram_ctrl_v4_0_9 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/d95a/hdl/axi_bram_ctrl_v4_0_rfs.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_axi_bram_ctrl_0_0/sim/zynq_design_1_axi_bram_ctrl_0_0.vhd" \
+-endlib
+-makelib ies/axi_lite_ipif_v3_0_4 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/0ba0/hdl/axi_lite_ipif_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/lib_cdc_v1_0_2 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/52cb/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib ies/interrupt_control_v3_1_4 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/e956/hdl/interrupt_control_v3_1_vh_rfs.vhd" \
+-endlib
+-makelib ies/axi_gpio_v2_0_12 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/475c/hdl/axi_gpio_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_axi_gpio_0_0/sim/zynq_design_1_axi_gpio_0_0.vhd" \
+-endlib
+-makelib ies/proc_sys_reset_v5_0_10 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/04b4/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_rst_ps7_0_100M_0/sim/zynq_design_1_rst_ps7_0_100M_0.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_axi_bram_ctrl_0_bram_0/sim/zynq_design_1_axi_bram_ctrl_0_bram_0.v" \
+-endlib
+-makelib ies/generic_baseblocks_v2_1_0 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/7ee0/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/axi_infrastructure_v1_1_0 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/7e3a/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies/axi_register_slice_v2_1_10 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/7efe/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/fifo_generator_v13_1_2 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/a807/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies/fifo_generator_v13_1_2 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/a807/hdl/fifo_generator_v13_1_rfs.vhd" \
+-endlib
+-makelib ies/fifo_generator_v13_1_2 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/a807/hdl/fifo_generator_v13_1_rfs.v" \
+-endlib
+-makelib ies/axi_data_fifo_v2_1_9 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/10b8/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/axi_crossbar_v2_1_11 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/d552/hdl/axi_crossbar_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_xbar_0/sim/zynq_design_1_xbar_0.v" \
+-endlib
+-makelib ies/axi_protocol_converter_v2_1_10 \
+  "../../../../lab1.srcs/sources_1/bd/zynq_design_1/ipshared/4a8b/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_auto_pc_0/sim/zynq_design_1_auto_pc_0.v" \
+  "../../../bd/zynq_design_1/ip/zynq_design_1_auto_pc_1/sim/zynq_design_1_auto_pc_1.v" \
+  "../../../bd/zynq_design_1/hdl/zynq_design_1.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ lab1.sdk/zynq_design/Debug/src/zynqdesign.d
@@ -0,0 +1,18 @@
+src/zynqdesign.o src/zynqdesign.o: ../src/zynqdesign.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../zynq_design_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
